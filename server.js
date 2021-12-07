1d55dcdcbc6c5312ef6850bea4de91985340ffac
Meredith Coyne <meredithleigh.coyne@gmail.com>
2021-12-06 21:52:19 -0500

    updated server file

--- server.js
+++ server.js
@@ -1,18 +1,27 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const logger = require("morgan");
+const compression = require("compression");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
+app.use(logger("dev"));
+app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
+// mongo development 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
+
+
 });
 
 // routes
